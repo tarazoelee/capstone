1ef023fc80856b9aef55d26b9258b6c93aef1873
Tara Lee <taralee@dyn172-30-144-51.wireless.uwo.pri>
2023-11-20 15:26:40 -0500

    formatting

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //const firebase = require('firebase');
 
-app.use("/api/test", users);
+app.use("/api/users", users);
 
 app.get("/api", (request, response) => {
   response.send("Hello world from Express!");
 });
 
 app.listen(1234, () => {
-  console.log("Running on port 1234!");
+  consle.log("Running on port 1234!");
 });
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,20 +3,16 @@ const router = express.Router();
 const { db } = require("../../util/admin.js");
 
 // Sample API route
-router.get('/users', (req, res) => {
-   try {
-    getUsers().then((data) => {
-      return res.status(200).send(data);
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send(error);
-  }
+router.get('/', (req, res) => {
+   getUsers().then((data) => {
+    return res.status(200).send(data);
+  });
 });
 
 async function getUsers(){
     console.log("hello")
-    const users = db.collection("users").get();
+    const collection = db.collection("users").doc("test");
+    let users = await collection.get();
     return users;
 }
 
