3cd6a5de68f5de7155e6d0f7195cbb2a5f28e647
Brayden Thompson <bthomp89@uwo.ca>
2023-11-21 19:48:26 -0500

    OM4450-7 Signup Functionality
    also cleaned up the repo a bit in here

---
+++ client/src/components/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Signup from "./Signup";
+import { AuthProvider } from "../contexts/AuthContext";
+
+function App() {
+  // state = {
+  //   data: null,
+  // };
+
+  // componentDidMount() {
+  //   this.callBackendAPI()
+  //     .then((res) => this.setState({ data: res.express }))
+  //     .catch((err) => console.log(err));
+  // }
+
+  // // fetching the GET route from the Express server which matches the GET route from server.js
+  // callBackendAPI = async () => {
+  //   const response = await fetch("/express_backend");
+  //   const body = await response.json();
+
+  //   if (response.status !== 200) {
+  //     throw Error(body.message);
+  //   }
+  //   return body;
+  // };
+
+  return (
+    <AuthProvider>
+      <Signup />
+    </AuthProvider>
+  );
+}
+
+export default App;
---
+++ client/src/contexts/AuthContext.js
@@ -0,0 +1,35 @@
+import React, { useContext, useState, useEffect } from "react";
+import { auth } from "../firebase";
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+
+  function signup(email, password) {
+    auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    signup,
+  };
+
+  return (
+    <div>
+      <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    </div>
+  );
+}
