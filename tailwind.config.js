70a5f70f16f527d56b897ee3364a5e835c2bcc5b
Tara Lee <taralee@Taras-MacBook-Pro.local>
2023-11-22 21:55:11 -0500

    login styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'myYellow':'#FFDE8A',
+      'fontyellow':'#8E6806',
+      ...colors
+    }
   },
   plugins: [],
+   safelist: [{
+            pattern: /(bg|text|border)-(myyellow)/
+        }]
 }
 
