74632a303eedf752479dad6693415bb37e025f97
Tara Lee <taralee@Taras-MacBook-Pro.local>
2023-11-23 15:51:15 -0500

    starting dash styling

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,7 +5,17 @@ export default function Dashboard() {
   const { currentUser } = useAuth();
 
   return (
-    <div>{currentUser.email}</div>
+    <div className="flex h-full">
+      <div className="flex flex-col w-1/3 items-center py-10 font-bold text-yellow-900 rounded-r-md bg-[#ffdd80]">
+       <div>{currentUser.email}</div>
+       <div>
+          <div>History</div>
+          <div>Home</div>
+       </div>
+      </div>
+      
+
+    </div>
   
   );
 }
