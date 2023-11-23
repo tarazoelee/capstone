b224cbf188e20d1a47f2f7147a1bbc2c60923536
Tara Lee <taralee@Taras-MacBook-Pro.local>
2023-11-22 21:39:24 -0500

    routing to signup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -25,18 +25,18 @@ export default function Login() {
     setLoading(false);
   }
   return (
-    <div className="flex mt-6 items-center">
-      <div className="flex flex-col w-1/3 border-2">
-        <div>Discover the daily news like never before</div>
+    <div className="flex h-full">
+      <div className="flex flex-col w-1/3 border-2 items-center justify-center bg-myYellow">
+        <div className="font-bold text-5xl px-32"> Discover the daily news like never before</div>
       </div>
-      <div className="flex flex-col w-2/3 border-2 justify-center items-center">
+      <div className="flex flex-col w-2/3 gap-6 border-2 justify-center items-center">
         <div className="font-bold text-3xl">
           Log In to DailyBytes
         </div>
         <div>
           Continue with Google 
         </div>
-        <div className="flex flex-col gap-7">
+        <div className="flex flex-col">
           <input
             className="mb-6 bg-none border-b border-black w-80"
             type="text"
@@ -54,14 +54,18 @@ export default function Login() {
             required
           />{" "}
           <button
-            className="bg-gray-200 h-10 text-sm"
+            className="bg-gray-200 h-10 text-sm rounded-md hover:bg-gray-300 ease-linear transition duration-100"
             type="submit"
             onClick={handleSubmit}
             disabled={loading}
           >
             Login
           </button>
+          <div className="text-center mt-2 text-xs">
+            Don't have an account? <a href='/signup'>Sign Up</a>
+          </div>
         </div>
+        
       </div>
     </div>
   );
