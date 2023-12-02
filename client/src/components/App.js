a822f6da4165bac513a364f7809efffa776c8ca7
Tara Lee <taralee@dyn172-30-144-150.wireless.uwo.pri>
2023-12-02 15:58:11 -0500

    set up express calls from mongo to front end

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,26 +7,6 @@ import Dashboard from "./Dashboard";
 import Profile from "./Profile";
 
 function App() {
-  // state = {
-  //   data: null,
-  // };
-
-  // componentDidMount() {
-  //   this.callBackendAPI()
-  //     .then((res) => this.setState({ data: res.express }))
-  //     .catch((err) => console.log(err));
-  // }
-
-  // // fetching the GET route from the Express server which matches the GET route from server.js
-  // callBackendAPI = async () => {
-  //   const response = await fetch("/express_backend");
-  //   const body = await response.json();
-
-  //   if (response.status !== 200) {
-  //     throw Error(body.message);
-  //   }
-  //   return body;
-  // };
 
   return (
     <div className="w-screen h-screen" >
