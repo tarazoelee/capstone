7adcd4a511f14e90f48d5eb6028ac8aaff95416e
Ben Fletcher <71139046+benf1etcher@users.noreply.github.com>
2024-02-01 11:56:14 -0500

    adding hovering styling

--- client/src/components/CreateProfile.js
+++ client/src/components/CreateProfile.js
@@ -138,7 +138,7 @@ function CreateProfile() {
         </div>
         <div className="flex gap-10 flex-wrap items-center justify-center text-gray-700">
           <div
-            className="bg-orange-200 w-32 px-6 py-2 text-center rounded-med rounded"
+            className="bg-orange-400 w-32 px-6 py-2 text-center rounded-med rounded hover:bg-orange-300 cursor-pointer"
             id="2 min"
             onClick={() => selectLength("2 min")}
           >
@@ -146,7 +146,7 @@ function CreateProfile() {
             1-2 min
           </div>
           <div
-            className="bg-orange-200 w-32 px-6 py-2 text-center rounded-med rounded"
+            className="bg-orange-200 w-32 px-6 py-2 text-center rounded-med rounded hover:bg-orange-300 cursor-pointer"
             id="5 min"
             onClick={() => selectLength("5 min")}
           >
@@ -154,14 +154,14 @@ function CreateProfile() {
             2-5 min{" "}
           </div>
           <div
-            className="bg-orange-200 w-32 px-6 py-2 text-center rounded-med rounded"
+            className="bg-orange-200 w-32 px-6 py-2 text-center rounded-med rounded hover:bg-orange-300 cursor-pointer"
             id="10 min"
             onClick={() => selectLength("10 min")}
           >
             5-10 min
           </div>
           <div
-            className="bg-orange-200 w-32 px-6 py-2 text-center rounded-med rounded"
+            className="bg-orange-200 w-32 px-6 py-2 text-center rounded-med rounded hover:bg-orange-300 cursor-pointer"
             id="20 min"
             onClick={() => selectLength("20 min")}
           >
