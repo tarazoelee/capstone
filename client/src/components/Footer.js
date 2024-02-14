7490f6c2f1d5262bdb6ea479102af6b9f5d9c5c4
Ben Fletcher <71139046+benf1etcher@users.noreply.github.com>
2024-02-14 17:43:52 -0500

    footer styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 export default function Footer(){
 
  return(
-     <div>
+     <div className='bg-gray-100 py-32 px-56'>
         This is our footer
      </div>
  );
