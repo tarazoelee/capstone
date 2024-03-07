bcfe76ef10c200c8397e6712fd6b736f38a8b233
Jhader2 <71098789+Jhader2@users.noreply.github.com>
2024-03-07 16:14:12 -0500

    footer styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 export default function Footer(){
+    const nav = useNavigate();
+
+    function navContactPage() {
+        nav("/contact-us");
+    }
+
+    function navProfile() {
+        nav("/profile");
+      }
 
  return(
-     <div className='bg-gray-100 py-32 px-56'>
-        This is our footer
+     <div className='bg-orange-50 py-24 px-56 text-black flex'>
+        <div onClick={navContactPage}>Contact Us</div>
+        <div onClick={navContactPage}>Profile</div>
      </div>
  );
 }
