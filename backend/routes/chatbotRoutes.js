457fd2aec762933c667bbb18d49bb7d79065e8d8
azhitkev <azhitkev@uwo.ca>
2024-03-14 20:33:07 -0400

    took out unneeded route

--- backend/routes/chatbotRoutes.js
+++ backend/routes/chatbotRoutes.js
@@ -21,31 +21,6 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-
-app.post("/summary", async (req, res) => {
-  try {
-    messageToProcess = req.body.message;
-    const articleInfo = `${messageToProcess}`;
-    const message =
-      `CONTEXT: Put the following articles into an interesting news format that summarizes the articles and can be read by one person. This is the information that you must summarize:` +
-      articleInfo;
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content: message,
-        },
-      ],
-      temperature: 0,
-      max_tokens: 1000,
-    });
-    res.send(response);
-  } catch (err) {
-    res.status(500).json(err.message);
-  }
-});
-
 app.get("/test", async (req, res) => {
   try {
     const result = await getTopicCombinations();
