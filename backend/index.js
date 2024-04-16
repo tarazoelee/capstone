a164a086ef9c5ecce4652f33912b5274d7c8d0de
Tara Lee <taralee@taras-mbp.home>
2024-04-16 17:54:44 -0400

    mern frontend link rewrite

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["capstone-frontend-tan.vercel.app"],
+    origin: ["https://capstone-frontend-tan.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   }),
